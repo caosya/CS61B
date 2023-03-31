623955661943410f8085bc376e4b5cd6a8512ac3
caosy <1960552705@qq.com>
2023-04-01 01:57:46 +0800

    lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -7,9 +7,7 @@ public class SquarePrimesTest {
 
     /**
      * Here is a test for isPrime method. Try running it.
-     * It passes, but the starter code implementation of isPrime
-     * is broken. Write your own JUnit Test to try to uncover the bug!
-     */
+      */
     @Test
     public void testSquarePrimesSimple() {
         IntList lst = IntList.of(14, 15, 16, 17, 18);
@@ -17,4 +15,35 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesSimple1() {
+        int[] arr = new int[10];
+        for(int i = 0; i < 10; i++) {
+            arr[i] = (int)(Math.random()*100);
+        }
+        StringBuffer target = new StringBuffer();
+        for(int i = 0; i < 10; i++) {
+            boolean currElemIsPrime = Primes.isPrime(arr[i]);
+            if(currElemIsPrime){
+                target.append(arr[i]*arr[i]).append(" -> ");
+            }else {
+                target.append(arr[i]).append(" -> ");
+            }
+        }
+        target.delete(target.length()-4,target.length());
+        IntList lst = IntList.of(arr);
+        boolean changed = IntListExercises.squarePrimes(lst);
+
+        assertEquals(target.toString(), lst.toString());
+//        assertTrue(changed);
+    }
+    @Test
+    public void testSquarePrimesSimple2() {
+        for(int i = 0; i < 50; i++) {
+            testSquarePrimesSimple1();
+        }
+    }
 }
+
+
